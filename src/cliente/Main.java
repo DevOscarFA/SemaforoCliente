0dba872b8700ed878af3ea965bb6c11607f3268b
Oscar Fernandez <oscarfdzalz@gmail.com>
2017-09-30 19:08:47 -0500

    Se crea proyecto semaforo cliente

---
+++ src/cliente/Main.java
@@ -0,0 +1,15 @@
+package cliente;
+
+import cliente.presentacion.Modelo;
+
+/**
+ * Punto de inicio del programa contro, semaforo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Modelo modelo = new Modelo();
+        modelo.iniciar();
+    }
+
+}
